1b883f0fcf0aabae18849f9ec530f5264c0b422a
Votre nom ou pseudo <lel.julien@laposte.net>
2020-03-27 16:11:53 +0100

    Rectification

--- Client/src/interfaces/IClient.java
+++ Client/src/interfaces/IClient.java
@@ -8,7 +8,7 @@ public interface IClient extends Remote{
 
 	public IClient connexionClient(String mail, String mdp) throws RemoteException, SQLException;
 	
-	public int recuperePanier() throws RemoteException, SQLException;
+	public IPanier recuperePanier() throws RemoteException, SQLException;
 	
 	public int getId() throws RemoteException;
 	
